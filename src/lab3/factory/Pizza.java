0f930c17d4a5fb98d4c19ca850b55f82acc9779c
IoanaDiac <69198000+IoanaDiac@users.noreply.github.com>
2020-11-30 23:48:51 +0200

    lab3- delegation, decorator,factory

---
+++ src/lab3/factory/Pizza.java
@@ -0,0 +1,28 @@
+package lab3.factory;
+
+public class Pizza {
+    private  String name;
+    private String dough;
+
+    @Override
+    public String toString(){
+        return "Pizza{"+" name= "+name+", dough="+dough+" }";
+    }
+
+    public Pizza(String name, String dough) {
+        this.name = name;
+        this.dough = dough;
+    }
+
+    public void bake(){
+        System.out.println("bake");
+    }
+
+    public void slice(){
+        System.out.println("slice");
+    }
+
+    public void box(){
+        System.out.println("box");
+    }
+}
